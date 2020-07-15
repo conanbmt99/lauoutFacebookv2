353347c64ba34c8218971c3671d878fb127e6b94
conanbmt99 <nguyenminh020299@gmail.com>
2020-07-15 18:24:40 +0700

    updeat

--- app/src/main/java/com/exam/lauoutfacebook/MainActivity.java
+++ app/src/main/java/com/exam/lauoutfacebook/MainActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 Button bt;
+EditText et;
+TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
