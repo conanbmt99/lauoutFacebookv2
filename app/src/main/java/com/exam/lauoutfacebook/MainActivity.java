6bedfd9a9b01cfbcda96c04ce1ff671bd8c20807
Minh <nguyenminh020299@gmail.com>
2020-07-15 17:17:21 +0700

    Initial commit

---
+++ app/src/main/java/com/exam/lauoutfacebook/MainActivity.java
@@ -0,0 +1,15 @@
+package com.exam.lauoutfacebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button bt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
